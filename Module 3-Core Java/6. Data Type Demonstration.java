627d6f762f630aad0d2da06e0d51f516758f4dd2
Monisha Kumar <kmonisha187@gmail.com>
2026-06-02 15:35:23 +0530

    Create 6. Data Type Demonstration.java

---
+++ Module 3-Core Java/6. Data Type Demonstration.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.5f;
+        double c = 20.75;
+        char d = 'A';
+        boolean e = true;
+
+        System.out.println("Int: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Char: " + d);
+        System.out.println("Boolean: " + e);
+    }
+}
