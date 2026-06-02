f5c4b9c54eea8b829b9102845c1f80d9222d363c
Monisha Kumar <kmonisha187@gmail.com>
2026-06-02 15:46:51 +0530

    Create 22. File Writing.java

---
+++ Module 3-Core Java/22. File Writing.java
@@ -0,0 +1,17 @@
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class FileWriting {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        FileWriter fw = new FileWriter("output.txt");
+        fw.write(text);
+        fw.close();
+
+        System.out.println("Data written successfully");
+    }
+}
