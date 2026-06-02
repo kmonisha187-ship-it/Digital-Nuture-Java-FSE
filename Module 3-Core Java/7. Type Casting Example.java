a1993e816ec86a33fed9c384ab4d6e734d4a771b
Monisha Kumar <kmonisha187@gmail.com>
2026-06-02 15:35:42 +0530

    Create 7. Type Casting Example.java

---
+++ Module 3-Core Java/7. Type Casting Example.java
@@ -0,0 +1,12 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d = 25.89;
+        int i = (int) d;
+
+        int num = 50;
+        double dd = num;
+
+        System.out.println("Double to Int: " + i);
+        System.out.println("Int to Double: " + dd);
+    }
+}
