6c7fe745ce1983900249be91f7a9783d9e41c9f4
Monisha Kumar <kmonisha187@gmail.com>
2026-06-02 16:00:30 +0530

    Create Server.java

---
+++ Module 3-Core Java/35. TCP Client-Server Chat/Server.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket ss = new ServerSocket(5000);
+
+        System.out.println("Waiting for Client...");
+
+        Socket s = ss.accept();
+
+        DataInputStream dis =
+                new DataInputStream(s.getInputStream());
+
+        String msg = dis.readUTF();
+
+        System.out.println("Client: " + msg);
+
+        ss.close();
+    }
+}
